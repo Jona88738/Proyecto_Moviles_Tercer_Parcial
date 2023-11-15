38a1d66bf245a4e8a47326fdfb541468255430ab
Jonathan <jona88738@gmail.com>
2023-11-15 16:07:57 -0600

    integracion de layout

--- Proyecto/app/src/main/java/com/example/proyecto/ui/home/HomeFragment.java
+++ Proyecto/app/src/main/java/com/example/proyecto/ui/home/HomeFragment.java
@@ -12,20 +12,24 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.proyecto.MainActivity;
 import com.example.proyecto.MenuLateral;
 import com.example.proyecto.PlatillosClass;
+import com.example.proyecto.R;
 import com.example.proyecto.Register_activity;
 import com.example.proyecto.Usuario;
 import com.example.proyecto.databinding.FragmentHomeBinding;
+import com.example.proyecto.fragment_cupones;
 
 public class HomeFragment extends Fragment {
 
 
     private FragmentHomeBinding binding;
     private Button OrdenaYa;
+    private Button Cupones;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +39,17 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
 
         OrdenaYa = binding.btnOrdenaYa;
+
+        Cupones =binding.btnCupones;
+        Cupones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction t = getParentFragmentManager().beginTransaction();
+                t.replace(R.id.Constraint_hom,new fragment_cupones());
+
+                t.commit();
+            }
+        });
 
         View root = binding.getRoot();
         OrdenaYa.setOnClickListener(v -> {
